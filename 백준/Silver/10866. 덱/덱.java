aa464392844e81915d80c4cc8efd20aa7833da4a
장서현 <68838251+rxjang@users.noreply.github.com>
2022-12-02 10:47:11 +0900

    [Silver IV] Title: 덱, Time: 188 ms, Memory: 18084 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.EmptyStackException;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int last = 0;
+         
+        int num = Integer.parseInt(br.readLine());
+        Deque<Integer> que = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+         
+        for (int i = 0; i < num; i++) {
+             String input = br.readLine();
+             if (input.startsWith("push_front")) {
+                 st = new StringTokenizer(input, " ");
+                 st.nextToken();
+                 last = Integer.parseInt(st.nextToken());
+                 que.addFirst(last);
+             } else if (input.startsWith("push_back")) {
+                 st = new StringTokenizer(input, " ");
+                 st.nextToken();
+                 last = Integer.parseInt(st.nextToken());
+                 que.addLast(last);
+             } else if (input.startsWith("pop_front")) {
+                 sb.append(que.isEmpty() ? -1 : que.poll())
+                     .append('\n');
+             } else if (input.startsWith("pop_back")) {
+                 sb.append(que.isEmpty() ? -1 : que.pollLast())
+                     .append('\n');
+             } else if (input.startsWith("size")) {
+                 sb.append(que.size())
+                     .append('\n');
+             } else if (input.startsWith("empty")) {
+                 sb.append(que.isEmpty() ? 1 : 0)
+                     .append('\n');
+             } else if (input.startsWith("front")) {
+                 sb.append(que.isEmpty() ? -1 : que.peek())
+                     .append('\n');
+             } else if (input.startsWith("back")) {
+                 sb.append(que.isEmpty() ? -1 : que.peekLast())
+                     .append('\n');
+             } 
+        }
+        System.out.println(sb);
+     }
+}
